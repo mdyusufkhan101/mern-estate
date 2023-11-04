8b21c01dc480f2031b80415eebfed6b870fdba3e
Yusuf <yusufkhanmd.1234@gmail.com>
2023-11-04 19:20:00 +0530

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+
+  <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<Signin />} />
@@ -15,4 +19,6 @@ export default function App() {
       <Route path="/profile" element={<Profile />} />
     </Routes>
   </BrowserRouter>
+
+  )
 }
