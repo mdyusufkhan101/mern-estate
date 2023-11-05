52d852fdd2ed2edecd71037524524be07c8843ae
Yusuf <yusufkhanmd.1234@gmail.com>
2023-11-05 19:13:12 +0530

    connected to databse

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const password = encodeURIComponent(DB_PASSWORD);
+
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${password}@mern-estate.erp2ou1.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-});
+})
